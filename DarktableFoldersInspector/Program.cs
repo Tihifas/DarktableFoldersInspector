eb838ad390899c3ca196efb2d9f1a7905264898b
Tihifas <tdbratberg@gmail.com>
2025-07-09 18:19:57 +0200

    Add project files.

---
+++ DarktableFoldersInspector/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DarktableFoldersInspector;
+using System.IO;
+
+DirectoryInfo darktableLibraryRootDI = new DirectoryInfo("F:\\Canon EOS R10\\Darktable Library");
+if(!darktableLibraryRootDI.Exists) 
+{
+    Console.WriteLine("Darktable Library root directory does not exist. Press eny key to close");
+    Console.ReadKey();
+    return 3;
+}
+
+FolderHelper.PrintFolderAndAllSubfoldersNames(darktableLibraryRootDI);
+
+Console.WriteLine("Job finished. Press eny key to close.");
+Console.ReadKey();
+return 0;
+
